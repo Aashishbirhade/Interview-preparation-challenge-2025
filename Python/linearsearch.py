54a3f86bf6af10707b35dfbaa9eed8f281f24eae
Aashishbirhade <aashishbirhade@gmail.com>
2025-11-24 21:18:24 +0530

    Day 2

---
+++ Python/linearsearch.py
@@ -0,0 +1,5 @@
+a = [1,2,2,3,4,4,5,6,78,666]
+k = 6
+for i in (a):
+    if i== k:
+        print("key is present")
---
+++ Python/reverse_string.py
@@ -0,0 +1,5 @@
+a ="Aashish"
+rev =""
+for i in range(len(a)):
+    rev = a[i] + rev
+print(rev)
